bf0880b4b917513c7d1463cc5c530c4d9b1bb71e
Umang Srivastava <Umang.Srivastava@csgi.com>
2023-05-29 19:57:57 +0530

    Added #36: valid sudoku

---
+++ arrayandhashing/36_valid_sudoku.go
@@ -0,0 +1,35 @@
+package arrayandhashing
+
+func isValidSudoku(board [][]byte) bool {
+	rowMap := make([]map[byte]bool, 9)
+	initializeMaps(rowMap)
+	columnMap := make([]map[byte]bool, 9)
+	initializeMaps(columnMap)
+	boardMap := make([][]map[byte]bool, 3)
+	for rIndex, grid := range board {
+		for cIndex := range grid {
+			boardMap[rIndex][cIndex] = make(map[byte]bool)
+		}
+	}
+	for row, rowVal := range board {
+		for col, colVal := range rowVal {
+			if colVal == '.' {
+				continue
+			}
+			rIndexForGrid, cIndexForGrid := row/3, col/3
+			if rowMap[row][colVal] || columnMap[col][colVal] || boardMap[rIndexForGrid][cIndexForGrid][colVal] {
+				return false
+			}
+			rowMap[row][colVal] = true
+			columnMap[col][colVal] = true
+			boardMap[rIndexForGrid][cIndexForGrid][colVal] = true
+		}
+	}
+	return true
+}
+
+func initializeMaps(arr []map[byte]bool) {
+	for index := range arr {
+		arr[index] = make(map[byte]bool)
+	}
+}
