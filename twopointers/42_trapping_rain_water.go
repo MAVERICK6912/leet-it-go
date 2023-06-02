46e6ffe6569fc92532e7dec866b62dc0c4d52b9d
Umang Srivastava <Umang.Srivastava@csgi.com>
2023-06-03 00:27:19 +0530

    Added: #42 Trapping Rain Water

---
+++ twopointers/42_trapping_rain_water.go
@@ -0,0 +1,33 @@
+package twopointers
+
+func trap(input []int) int {
+	if len(input) == 0 {
+		return 0
+	}
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	// min := func(a, b int) int {
+	// 	if a < b {
+	// 		return a
+	// 	}
+	// 	return b
+	// }
+	left, right, res := 0, len(input)-1, 0
+	maxLeft, maxRight := input[left], input[right]
+	for left < right {
+		if maxLeft < maxRight {
+			left += 1
+			maxLeft = max(maxLeft, input[left])
+			res += maxLeft - input[left]
+		} else {
+			right -= 1
+			maxRight = max(maxRight, input[right])
+			res += maxRight - input[right]
+		}
+	}
+	return res
+}
